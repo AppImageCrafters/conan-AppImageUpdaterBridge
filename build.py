da99bbff08df566f71debafaa15b0632865c1d2e
Alexis Lopez Zubieta <contact@azubieta.net>
2019-06-24 14:41:27 -0500

    Only build for x86

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from cpt.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    remotes = [("https://api.bintray.com/conan/bincrafters/public-conan", "yes", "bincrafters"),
+               ("https://api.bintray.com/conan/conan-community/conan", "yes", "conan-community")]
+
+    builder = ConanMultiPackager(build_policy="outdated", remotes=remotes, build_types=["Release"], archs=["x86_64"])
     builder.add_common_builds(shared_option_name="AppImageUpdaterBridge:shared")
     builder.run()
